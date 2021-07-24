aff68fbd3e06de2b57db02a648cb12b6f861e6a2
rubenhidayat <rubenhidayat02@gmail.com>
2021-07-24 11:05:43 +0700

    untuk nampilke data sesuai bulan dan tahun tapi tabel ny belum

--- include/p_laporan.php
+++ include/p_laporan.php
@@ -1,27 +1,18 @@
 <?php 
+session_start();
 include 'functions.php';
 
-$alternatif = query("SELECT penilaian.idproduk, produk.namaproduk FROM produk, penilaian WHERE penilaian.idproduk=produk.idproduk");
-
-$kriteria =  query("SELECT penilaian.idkriteria, kriteria.namakriteria, penilaian.nilai, kriteria.bobot FROM kriteria, penilaian WHERE kriteria.idkriteria = penilaian.idkriteria");
-
-$nilai = query("SELECT idproduk, idkriteria, nilai FROM penilaian");
-
-function data_array($alternatif, $kriteria, $nilai)
-{
-    for ($i = 0; $i < count($alternatif); $i++) {
-        for ($j = 0; $j < count($kriteria); $j++) {
-            foreach ($sele as $n) {
-                if ($n['idproduk'] == $alternatif[$i]['idproduk'] && $n['idkriteria'] == $kriteria[$j]['idkriteria']) {
-                    $data_array[$i][$j] = $n['nilai'];
-                }
-            }
-        }
-    }
-    return $data_array;
-    var_dump($data_array);
+if (isset($_POST['submitdata'])) {
+    $bulan = $_POST['periodebulan'];
+    $tahun = $_POST['periodetahun'];
+    $id = "SELECT * FROM PENILAIAN WHERE periodebulan='$bulan' AND periodetahu='$tahun' ";
+    echo "$id";
     exit;
+    
 }
 
 
  ?>
+
+
+
--- laporan.php
+++ laporan.php
@@ -11,79 +11,134 @@ include 'include/functions.php';
 
 <div class="content">
 	
-				<div class="card-header card-header-primary">
-					<h4 class="card-title">
-						Masukkan Bulan dan Tahun
-					</h4>
-				</div>
-				<div class="card-body">
-					<?php if (isset($_GET['failed'])) { ?>
-                            <div class="alert alert-danger col-4">
-                                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
-                                    <i class="material-icons">close</i>
-                                </button>
-                                <span>
-                                    <b> Data Gagal Diproses! </b></span>
-                            </div>
+	<div class="card-header card-header-primary">
+		<h4 class="card-title">
+			Masukkan Bulan dan Tahun
+		</h4>
+	</div>
+	<div class="card-body">
+		<?php if (isset($_GET['failed'])) { ?>
+            <div class="alert alert-danger col-4">
+                <button type="button" class="close" data-dismiss="alert" aria-label="Close">
+                    <i class="material-icons">close</i>
+                </button>
+                    <span>
+                        <b> Data Gagal Diproses! </b>
+                    </span>
+            </div>
                         <?php } ?>
-                        <form action="" method="POST">
-                        	<div class="row">
-                                    <div class="col-md-8">
-                                        <div class="form-group">
-                                            <label class="bmd-label-floating">Periode Bulan</label>
-                                            <select name="periodebulan" class="form-control">
-                                                <option value="0">--Pilih Bulan--</option>
-                                                <option value="januari">januari
-                                                </option>
-                                                <option value="februari">februari
-                                                </option>
-                                                <option value="maret">maret
-                                                </option>
-                                                <option value="april">april
-                                                </option>
-                                                <option value="mei">mei
-                                                </option>
-                                                <option value="juni">juni
-                                                </option>
-                                                <option value="juli">juli
-                                                </option>
-                                                <option value="agustus">agustus
-                                                </option>
-                                                <option value="september">september
-                                                </option>
-                                                <option value="oktober">oktober
-                                                </option>
-                                                <option value="november">november
-                                                </option>
-                                                <option value="desember">desember
-                                                </option>        
-                                                </select>
-                                        </div>
-                                    </div>       
-                                </div>
 
-                                <div class="row">
-                                    <div class="col-md-8">
-                                        <div class="form-group">
-                                            <label class="bmd-label-floating">Periode Tahun</label>
-                                            <select name="periodetahun" class="form-control">
-                                            <option value="">-Pilih Tahun-</option>
-                                                  <?php 
-                                                    $y = date('Y');
-                                                    for ($i=2019; $i <=$y+5 ; $i++) { 
-                                                      echo "<option value='$i'>$i</option>";
-                                                    }
-                                                  ?>
-                                            </select>                                        
-                                        </div>
-                                    </div>       
-                                </div>
-                                <button type="submit" name="filter" class="btn btn-success pull-right">Filter</button>
-                                <div class="clearfix"></div> 
-                        </form>
-				</div>
+
+        <form action="laporan.php" method="POST">
+                    <select name="periodebulan" class="form-control">
+                        <option value="">-pilih bulan-</option>
+                        <option value="januari">januari</option>
+                        <option value="februari">februari</option>
+                        <option value="maret">maret</option>
+                        <option value="april">april</option>
+                        <option value="mei">mei</option>
+                        <option value="juni">juni</option>
+                        <option value="juli">juli</option>
+                        <option value="agustus">agustus</option>
+                        <option value="september">september</option>
+                        <option value="oktober">oktober</option>
+                        <option value="november">november</option>
+                        <option value="desember">desember</option>    
+                    </select>
+                    <br>
+
+                    <select name="periodetahun" class="form-control">
+                        <option value="">-Pilih tahun-</option>
+                          <?php 
+                            $y = date('Y');
+                            for ($i=2019; $i <=$y+5 ; $i++) { 
+                              echo "<option value='$i'>$i</option>";
+                            }
+                          ?>
+                    </select>
+                    <br>
+
+                    
+
+                    <br>
+                    <button type="submit">Cari</button>
+                    
+                </form>
+                <br>
+                <?php
+                    if ((isset($_POST['periodebulan']) && $_POST['periodebulan']!='') && (isset($_POST['periodetahun']) && $_POST['periodetahun']!='')) {
+                      $bulan = $_POST['periodebulan'];
+                      $tahun = $_POST['periodetahun'];
+                    }else{
+                      $bulan = date('m');
+                      $tahun = date('Y');
+                    }
+                ?>
+                <strong>Bulan :<?php echo$bulan;?>, Tahun: <?php echo $tahun;?></strong>      
+	</div>
+
+                
 </div>
-	
 
+<?php 
+$no = 1;
+
+if ((isset($_POST['periodebulan'])) && (isset($_POST['periodetahun']))) {
+    $bulan=$_POST['periodebulan'];
+    $tahun=$_POST['periodetahun'];
+    $sql = "SELECT * FROM PENILAIAN WHERE periodebulan=$bulan AND periodetahun=$tahun";
+}else{
+    $bulan = date('m');
+    $tahun = date('y');
+    $sql = "SELECT FROM PENILAIAN WHERE periodebulan=$bulan AND periodetahun=$tahun";
+}
+$result = mysqli_query($conn, $sql);
+
+
+?>
+
+<div class="content">
+    <div class="card-body">
+    <h4>Data Penjualan SPK bulan <?= $bulan ?> tahun <?= $tahun ?></h4>
+    <table class="table table-bordered table-striped mb-0">
+        <thead>
+            <tr>
+                <th>No</th>
+                <th>idpenilaian</th>
+                <th>idproduk</th>
+                <th>idkriteria</th>
+                <th>nilai</th>
+                <th>periode bulan</th>
+                <th>periode tahun</th>
+            </tr>
+        </thead>
+        <?php 
+        global $result;
+        while($row = mysqli_fetch_assoc($result)){?>
+            <tbody>
+                <tr>
+                    <td><?= $no++; ?></td>
+                    <td><?= $row['idpenilaian']; ?></td>
+                    <td><?= $row['idproduk']; ?></td>
+                    <td><?= $row['idkriteria']; ?></td>
+                    <td><?= $row['nilai']; ?></td>
+                    <td><?= $row['periodebulan']; ?></td>
+                    <td><?= $row['periodetahun']; ?></td>
+                </tr>
+            </tbody>
+        <?php } ?>
+
+        <?php if (mysqli_num_rows($result)) {?>
+            <tr><td colspan='7' text-align='center'><a href='laporan.php'></a></td></tr></tr>
+        <?php}else{?>
+            <tr><td>0result</td></tr>
+        <?php } ?>
+
+       
+        
+    </table>
+    <?php mysqli_close($conn); ?>
+    </div>
+</div>
 
 
